f7ab0cceae96b8c54911ff58b2cef678cad52b08
ErkanatImansharipov <erkanat-cfc11@inbox.ru>
2023-11-21 22:11:51 +0600

    first Hello World Commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a bool = true    // boolean (true/false)
+	var b int = 5        // integer
+	var c float32 = 3.14 // floating going namber
+	var d string = "Era" // string
+
+	fmt.Println("Boolean: ", a)
+	fmt.Println("Integer: ", b)
+	fmt.Println("Float: ", c)
+	fmt.Println("String: ", d)
+}
